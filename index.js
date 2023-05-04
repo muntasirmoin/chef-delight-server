63bb0200ea02315ca2ffb386762981a1bc66d3cf
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2023-05-04 22:59:49 +0600

    some data added

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ app.get('/chef', (req, res) =>{
 
 // chef selected id
 
-// app.get('/chef/:id', (req, res)=>{
-//     const id = parseInt(req.params.id);
-//     console.log(id);
-//     const selectedChef = chef.find(n => parseInt(n.id) === id);
-//     res.send(selectedChef);
-// })
+app.get('/chef/:id', (req, res)=>{
+    const id = parseInt(req.params.id);
+    console.log(id);
+    const selectedChef = chef.find(n => parseInt(n.id) === id);
+    res.send(selectedChef);
+})
 
 app.listen(port,()=>{
     console.log(`Chef Delight API is running on port: ${port}`);
